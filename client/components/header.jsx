c8b979fda9219a80f4b5c09e55e048d88a022daf
Todd Shinto <todd_shinto@yahoo.com>
2020-06-12 20:30:13 +0000

    header component

---
+++ client/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <h1>Wicked Sales <img src="/images/favicon.png" /></h1>
+    );
+  }
+}
