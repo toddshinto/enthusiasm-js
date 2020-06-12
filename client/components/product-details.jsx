12513fad624c2236bc76c6a36ea8cf060bd87556
Todd Shinto <todd_shinto@yahoo.com>
2020-06-12 22:15:10 +0000

    component did mount

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -7,4 +7,32 @@ export default class ProductDetails extends React.Component {
       product: null
     };
   }
+
+  componentDidMount() {
+    fetch('api/products/' + 'productId')
+      .then(response => response.json())
+      .then(data => this.setState({ product: data }));
+  }
+
+  render() {
+    const product = this.state.product;
+    const price = product.price.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    return this.state.product
+      ? <div>
+        <div>
+          <img src={product.image} />
+          <div className="card">
+            <div className="card-title">
+              <h3>{product.name}</h3>
+              <h5 className="text-secondary">{price}</h5>
+              <p className="card-text">{product.shortDescription}</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p>{product.longDescription}</p>
+        </div>
+      </div>
+      : <></>;
+  }
 }
