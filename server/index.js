ef3a1d0e50228f51c558dd3df34a74c2c4e61827
Todd Shinto <todd_shinto@yahoo.com>
2020-06-12 23:44:23 +0000

    cart end point returns empty array

--- server/index.js
+++ server/index.js
@@ -51,6 +51,13 @@ app.get('/api/products/:productId', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/cart', (req, res, next) => {
+  db.query('')
+    .then(result => {
+      res.json([]);
+    });
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
