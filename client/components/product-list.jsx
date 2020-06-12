7d71ce780df34addc0f6acf5acc201105b80b2dc
Todd Shinto <todd_shinto@yahoo.com>
2020-06-12 20:43:48 +0000

    initial empty state for product list

---
+++ client/components/product-list.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+    this.getProducts = this.getProducts.bind(this);
+  }
+
+  getProducts() {
+
+  }
+}
