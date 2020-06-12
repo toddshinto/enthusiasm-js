d228452f0b3790f76fe32f5a7fc307a1d8348daf
Todd Shinto <todd_shinto@yahoo.com>
2020-06-12 21:51:23 +0000

    return statement

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.get('/api/products/:productId', (req, res, next) => {
   db.query(sql, params)
     .then(result => {
       if (result.rows.length < 1) {
-        next(new ClientError(`cannot ${req.method} ${req.originalUrl}, product does not exist`, 404));
+        return next(new ClientError(`cannot ${req.method} ${req.originalUrl}, product does not exist`, 404));
       }
       return res.status(200).json(result.rows[0]);
     })
