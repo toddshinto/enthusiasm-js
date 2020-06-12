5084399f093ae4a3bb52be29c19aff2d437a0e18
Todd Shinto <todd_shinto@yahoo.com>
2020-06-12 21:26:49 +0000

    theater masks icon

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Header extends React.Component {
   render() {
     return (
-      <h5 className="bg-dark text-white pl-3">Wicked Sales <img src="/images/favicon.png" /></h5>
+      <h5 className="bg-dark text-white pl-3 pt-2 pb-2">Wicked Sales <i className="fas fa-theater-masks" /></h5>
     );
   }
 }
