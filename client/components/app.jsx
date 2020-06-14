6fa3d16683873f8a4db3f6f4a806617294e25e68
Todd Shinto <todd_shinto@yahoo.com>
2020-06-14 00:00:11 +0000

    fixed post

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -38,8 +38,12 @@ export default class App extends React.Component {
   }
 
   addToCart(product) {
-    fetch(`/api/cart/${product.productId}`, {
-      method: 'POST'
+    fetch('/api/cart', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(product)
     })
       .then(res => res.json())
       .then(data => {
