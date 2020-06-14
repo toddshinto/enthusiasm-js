ac7bd5eb5a4ad046f73bcd834e1e5cf5f10e6069
Todd Shinto <todd_shinto@yahoo.com>
2020-06-14 05:03:45 +0000

    checkout form

---
+++ client/components/checkout-form.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export default class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      creditCard: '',
+      shippingAddress: ''
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const orderInfo = {
+      name: this.state.name,
+      creditCard: this.state.creditCard,
+      shippingAddress: this.state.shippingAddress
+    };
+    this.props.placeOrder(orderInfo);
+    this.handleReset();
+  }
+
+  handleReset() {
+    this.setState({
+      name: '',
+      creditCard: '',
+      shippingAddress: ''
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="name" onChange={this.handleChange} placeholder="Name"></input>
+          <input type="text" name="creditCard" onChange={this.handleChange} placeholder="0000 0000 0000 0000"></input>
+          <textarea name="shippingAddress" onChange={this.handleChange} placeholder="Shipping Address"></textarea>
+        </form>
+      </div>
+    );
+  }
+}
