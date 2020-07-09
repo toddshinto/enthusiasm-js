1f87ffdd65678510eb8f5bd9fbc5d6d6c483cf29
Todd Shinto <todd_shinto@yahoo.com>
2020-07-09 12:34:24 +0000

    buttons turn red on hover

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -6,10 +6,10 @@ export default class ProductListItem extends React.Component {
     const price = Number((product.price / 100).toFixed(2)).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
     return (
       <div className="col-md-4 mb-3 product" >
-        <div className="card h-100 shadow-sm">
-          <img className="card-img-top" onClick={() => this.props.setView('details', { productId: product.productId })} src={product.image} style={{ height: 250, objectFit: 'contain', cursor: 'pointer' }}/>
+        <div className="card h-100 grey-shadow" onClick={() => this.props.setView('details', { productId: product.productId })} style={{ cursor: 'pointer' }}>
+          <img className="card-img-top" src={product.image} style={{ height: 250, objectFit: 'contain', cursor: 'pointer' }}/>
           <div className="card-body">
-            <h3 className="card-title" onClick={() => this.props.setView('details', { productId: product.productId })} style={{ cursor: 'pointer' }}>{product.name}</h3>
+            <h5 className="card-title">{product.name}</h5>
             <h6 className="card-subtitle text-secondary">{price}</h6>
             <p className="card-text mt-1">{product.shortDescription}</p>
           </div>
