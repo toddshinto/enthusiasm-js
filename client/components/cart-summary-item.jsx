5ef0f668139929b015ad8d2348e4f1dcd799bd30
Todd Shinto <todd_shinto@yahoo.com>
2020-07-13 23:18:21 +0000

    checkout items list width adj

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -6,7 +6,7 @@ export default class CartSummaryItem extends React.Component {
     const price = Number((item.price / 100).toFixed(2)).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
     return (
       <div className="shadow-sm row">
-        <div className="col-sm-12 col-md-3 text-center">
+        <div className="col-sm-12 col-md-5 text-center">
           <img src={item.image} className='summary-img'></img>
         </div>
         <div className="summary-desc col-sm-12 col-md-6 pb-3">
